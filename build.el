a767d115500d0b498da0f37833a9102572caa670
Asilata Bapat (peanut) <asilata@gmail.com>
2022-10-19 10:26:31 +1100

    made build options local.

--- build.el
+++ build.el
@@ -2,29 +2,50 @@
 (require 'ox-publish)
 (require 'subr-x)
 
+(let ((ab/html-head "<link rel=\"stylesheet\" href=\"css/stylesheet.css\">")
+      (ab/html-head-extra
+       (string-join 
+        '("<link href=\"https://fonts.googleapis.com/css?family=Neuton:400,400i,700,700i\" rel=\"stylesheet\">"
+          "<script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+     ga('create', '{{ site.analytics_id}}', 'auto');
+     ga('send', 'pageview');
+    </script>")))
+      (ab/html-postamble-format
+       '(("en" "<footer>
+    Page generated using %c. Last modified on %C. Source on <a href=\"https://github.com/asilata/asilata.github.io\">github</a>.
+    </footer>")))
+      (ab/latex-classes
+       '(("moderncv"
+          "\\documentclass[a4paper]{moderncv}
+          [NO-DEFAULT-PACKAGES]
+          \\usepackage{amsmath,amssymb}"
+          ("\\section{%s}" . "\\section*{%s}")
+          ("\\subsection{%s}" . "\\subsection*{%s}")
+          ("\\paragraph{%s}" . "\\paragraph*{%s}")
+          ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+      )
+
 (setq org-publish-project-alist
-      '(("website"
+      `(("website"
          :base-directory "./content"
          :publishing-directory "./public"
          :recursive t
-         :exclude "data"
+         :exclude "data\\|cv\\|stylesheet"
          :publishing-function org-html-publish-to-html
          :with-broken-links t
          :with-creator t
          :with-title nil
          :with-toc nil
-         :html-preamble make-header
          :html-head-include-default-style nil
          :html-head-include-scripts nil
-         :html-head "<link rel=\"stylesheet\" href=\"css/stylesheet.css\">"
-         :html-head-extra "<link href=\"https://fonts.googleapis.com/css?family=Neuton:400,400i,700,700i\" rel=\"stylesheet\"><script>
-   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-   ga('create', '{{ site.analytics_id}}', 'auto');
-   ga('send', 'pageview');
-  </script>"
+         :html-head ,ab/html-head
+         :html-head-extra ,ab/html-head-extra
+         :html-postamble-format ,ab/html-postamble-format
+         :html-preamble ab/make-html-preamble
          :section-numbers nil
          )
         ("assets"
@@ -43,38 +64,34 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
          :base-directory "./content/cv"
          :publishing-directory "./public/cv"
          :latex-hyperref-template nil
+         :latex-classes ,ab/latex-classes
          :publishing-function org-latex-publish-to-latex
          )
         )
-      )
+      ))
 
-(setq org-html-postamble t
-      org-html-postamble-format '(("en" "<footer>
-Page generated using %c. Last modified on %C. Source on <a href=\"https://github.com/asilata/asilata.github.io\">github</a>.
-</footer>")))
-
-(defun make-header (options)
+(defun ab/make-html-preamble (options)
   (concat "<header>"
           "<h1><a href=\"/\">Asilata Bapat</a></h1>"
           "<nav>"
-          (make-navbar)
+          (ab/make-navbar)
           "</nav></header>"))
 
-(defun make-navbar ()
+(defun ab/make-navbar ()
   (let ((lst '("Home" "Teaching" "Research" "Activities"))
         ;; (title (cadar (org-collect-keywords '("TITLE"))))
         )
     (concat "<ul>\n"
             (string-join
-             (mapcar (lambda (x) (make-nav-item x)) lst) "\n")
+             (mapcar (lambda (x) (ab/make-nav-item x)) lst) "\n")
             "\n</ul>")))
 
-(defun make-nav-item (str)
+(defun ab/make-nav-item (str)
   (let ((slug (if (string-equal str "Home") "index" (downcase str))))
     (format "<li><a href=\"%s.html\">%s</a></li>"
             slug
             str)))  
-(defun make-nav-item-fancy (str title)
+(defun ab/make-nav-item-fancy (str title)
   (let ((slug (if (string-equal str "Home") "index" (downcase str))))
     (format "<li><a href=\"%s.html\"%s>%s</a></li>"
             slug
@@ -149,18 +166,6 @@ Page generated using %c. Last modified on %C. Source on <a href=\"https://github
             (if comment (format " (%s)" comment) "")
             (if links (format " (%s)" links) ""))
             ))
-
-(add-to-list 'org-latex-classes
-             `("moderncv"
-               ,(string-join
-                 '("\\documentclass[a4paper]{moderncv}"
-                   "[NO-DEFAULT-PACKAGES]"
-                   "\\usepackage{amsmath,amssymb}")
-                 "\n")
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 (setq org-confirm-babel-evaluate nil)
 (setq make-backup-files nil)
